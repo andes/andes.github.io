97e16f72a780403b3f343cf8ffd03f968e38b29b
Julio Nicolás Santarelli <santarellijulio@gmail.com>
2017-07-27 13:10:51 -0300

    datos mapa

--- js/main.js
+++ js/main.js
@@ -2,36 +2,36 @@ console.log("asd");
 (function (window) {
 
     var locations = [{
-        nombre: 'Castro Rendom',
-        direccion: 'Buenos Aires 450',
-        telefono: '0299-3000000',
+        nombre: 'Hospital Ciudad de Centenario',
+        direccion: 'Av. del Libertador 700, 8309 Centenario, Neuquén',
+        telefono: '0299 489-8862',
         location: {
             lat: -38.82152,
             lng: -68.13699
         }
     },
     {
-        nombre: 'Heller',
-        direccion: 'Buenos Aires 450',
-        telefono: '0299-3000000',
+        nombre: 'Hospital Heller',
+        direccion: 'Cnel. Godoy E. 1549-1615',
+        telefono: '0299 422-6532',
         location: {
             lat: -38.9379,
             lng: -68.1171
         }
     },
     {
-        nombre: 'Hospital B',
-        direccion: 'Buenos Aires 450',
-        telefono: '0299-3000000',
+        nombre: 'Hospital Bouquet Roldán',
+        direccion: 'Dr. Teodoro Luis Planas 1555, Neuquén',
+        telefono: '0299 443-8181',
         location: {
             lat: -38.95074,
             lng: -68.07033
         }
     },
     {
-        nombre: 'HOSPITAL A',
-        direccion: 'Buenos Aires 450',
-        telefono: '0299-3000000',
+        nombre: 'Hospital Provincial Neuquen "Dr. Castro Rendon"',
+        direccion: 'Buenos Aires 450, 8300 Neuquén',
+        telefono: '0299 449-0800',
         location: {
             lat: -38.95029,
             lng: -68.05686
